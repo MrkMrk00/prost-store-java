7b186528f7e43434cf064d3124231270f3098634
marek.hauschwitz <marek.hauschwitz@stud.uni-bamberg.de>
2022-11-30 00:31:47 +0100

    CSS separate bundle

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,11 @@
-const tailwindcss = require('tailwindcss');
-
 module.exports = {
     plugins: {
-        'tailwindcss/nesting': {}, 'postcss-preset-env': {features: { 'nesting-rules': false }}, tailwindcss: {}
+        'tailwindcss/nesting': {},
+        'postcss-preset-env': {
+            features: {
+                'nesting-rules': false,
+            },
+        },
+        tailwindcss: {},
     },
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,43 @@
 const { resolve } = require('path');
 const WatchExternalFilesPlugin = require('webpack-watch-files-plugin').default;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const assets = 'src/main/resources/static/build';
 
 module.exports = {
     mode: 'development',
-    entry: './frontend/index.js',
+    entry: {
+        bundle: './frontend/index.js',
+    },
     output: {
         path: resolve(__dirname, assets),
-        filename: 'bundle.js',
+        filename: '[name].js',
         library: {
             name: 'prostLib',
             type: 'var',
         },
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'bundle.css',
+        }),
+        new WatchExternalFilesPlugin({
+            files: ['src/main/resources/templates/**/*.html'],
+        }),
+        new CssMinimizerPlugin(),
+    ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
                 include: resolve(__dirname, 'frontend'),
-                use: ['style-loader', 'css-loader', 'postcss-loader'],
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader'
+                ],
+                sideEffects: true,
             },
         ],
     },
-    plugins: [
-        new WatchExternalFilesPlugin({
-            files: ['src/main/resources/templates/**/*.html'],
-        }),
-    ],
 };
