2c7e287d8332e9529ff15307dc344519250ada5b
marek.hauschwitz <marek.hauschwitz@stud.uni-bamberg.de>
2022-12-21 02:52:08 +0100

    Tests for Cart (the only important thing I guess)

--- src/test/java/de/unibamberg/dsam/group6/prost/ApplicationMainTests.java
+++ src/test/java/de/unibamberg/dsam/group6/prost/ApplicationMainTests.java
@@ -3,6 +3,8 @@ package de.unibamberg.dsam.group6.prost;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
+
 @SpringBootTest
 class ApplicationMainTests {
 
