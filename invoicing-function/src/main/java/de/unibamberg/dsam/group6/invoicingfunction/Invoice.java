7316bd40dcec86d87461e5f11006aa991ff08562
marek.hauschwitz <marek.hauschwitz@stud.uni-bamberg.de>
2023-01-20 14:36:08 +0100

    Invoicing gcloud function

---
+++ invoicing-function/src/main/java/de/unibamberg/dsam/group6/invoicingfunction/Invoice.java
@@ -0,0 +1,16 @@
+package de.unibamberg.dsam.group6.invoicingfunction;
+
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+
+import java.io.BufferedWriter;
+
+public class Invoice implements HttpFunction {
+
+    @Override
+    public void service(HttpRequest request, HttpResponse response) throws Exception {
+        BufferedWriter writer = response.getWriter();
+        writer.write("Hello World!");
+    }
+}
